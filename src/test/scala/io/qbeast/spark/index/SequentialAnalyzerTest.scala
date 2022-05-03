6177bb167347554e5608f893ad5911873a4b8882
jiaweihu <jiaweihu@jiaweihus-MacBook-Air.local>
2022-05-03 15:47:30 +0200

    Update test

--- src/test/scala/io/qbeast/spark/index/SequentialAnalyzerTest.scala
+++ src/test/scala/io/qbeast/spark/index/SequentialAnalyzerTest.scala
@@ -1,8 +1,25 @@
 package io.qbeast.spark.index
 
 import io.qbeast.spark.{QbeastIntegrationTestSpec, QbeastTable}
+import org.apache.spark.sql.SparkSession
 
 class SequentialAnalyzerTest extends QbeastIntegrationTestSpec {
+
+  def showMetrics(spark: SparkSession, path: String): Unit = {
+    // scalastyle:off println
+
+    val metrics = QbeastTable.forPath(spark, path).getIndexMetrics()
+
+    println(metrics)
+    println("Average maxWeight per level.")
+
+    metrics.cubeStatuses
+      .groupBy(cw => cw._1.depth)
+      .mapValues(m => m.values.map(st => st.maxWeight.fraction).sum / m.size)
+      .toSeq
+      .sortBy(_._1)
+      .foreach(println)
+  }
 
   it should "write data correctly using Sequential analyzer and show metrics" in
     withSparkAndTmpDir { (spark, tmpDir) =>
@@ -21,19 +38,7 @@ class SequentialAnalyzerTest extends QbeastIntegrationTestSpec {
         .option("analyzerImp", "sequential")
         .save(tmpDir)
 
-      // scalastyle:off println
-      val metrics = QbeastTable
-        .forPath(spark, tmpDir)
-        .getIndexMetrics()
-
-      println(metrics)
-
-      metrics.cubeStatuses
-        .groupBy(cw => cw._1.depth)
-        .mapValues(m => m.values.map(st => st.maxWeight.fraction).sum / m.size)
-        .toSeq
-        .sortBy(_._1)
-        .foreach(println)
+      showMetrics(spark, tmpDir)
 
       df.count() shouldBe spark.read.format("qbeast").load(tmpDir).count()
     }
@@ -52,24 +57,11 @@ class SequentialAnalyzerTest extends QbeastIntegrationTestSpec {
         df.write
           .format("qbeast")
           .option("columnsToIndex", "event_time,user_id,price")
-          .option("cubeSize", 5000)
+          .option("cubeSize", 50000)
           .option("analyzerImp", "piecewiseSeq")
           .save(tmpDir))
 
-      // scalastyle:off println
-      val metrics = QbeastTable
-        .forPath(spark, tmpDir)
-        .getIndexMetrics()
-
-      println(metrics)
-
-      metrics.cubeStatuses
-        .groupBy(cw => cw._1.depth)
-        .mapValues(m => m.values.map(st => st.maxWeight.fraction).sum / m.size)
-        .toSeq
-        .sortBy(_._1)
-        .foreach(println)
-
+      showMetrics(spark, tmpDir)
       df.count() shouldBe spark.read.format("qbeast").load(tmpDir).count()
     }
 }
