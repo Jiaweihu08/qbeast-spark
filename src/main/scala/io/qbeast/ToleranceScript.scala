687dff90c4a095869b02b292ed3a6d3be88d0e44
jiaweihu <jiaweihu@jiaweihus-MacBook-Air.local>
2022-06-13 17:52:47 +0200

    Create script for tolerance

--- src/main/scala/io/qbeast/ToleranceScript.scala
+++ src/main/scala/io/qbeast/ToleranceScript.scala
@@ -3,41 +3,39 @@
  */
 package io.qbeast
 
-import org.apache.spark.sql.{DataFrame, SparkSession}
-import org.apache.spark.sql.functions.{col, to_date, year}
+import io.qbeast.spark.index.query.FileExtractor
+import org.apache.spark.sql.functions.{expr, udf}
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 
+// scalastyle:off
 class ToleranceScript(spark: SparkSession) {
-  val sourcePrPath = "s3a://qbeast-research/github-archive/pr/"
-  val pr: DataFrame = spark.read.format("parquet").load(sourcePrPath)
-  val prWithYear: DataFrame = pr.withColumn("year", year(to_date(col("month_year"), "MM-yyyy")))
   val columnsToIndex = "repo_main_language,year"
+  val targetColumns = Seq("added_rows", "deleted_rows", "changed_files")
 
-  pr.createOrReplaceTempView("pr")
-
-  // Replace pr with prWithYear
-  // Index prWithYear using the Double Pass Implementation
-
-//  prWithYear.write
-//    .format("qbeast")
-//    .option("columnsToIndex", columnsToIndex)
-//    .save("s3a://qbeast-research/github-archive/qbeast-pr/double/original/")
-
-  def writeChangedFiles(): Unit = {
-    val doubleLimitedPath =
-      "s3a://qbeast-research/github-archive/qbeast-pr/double/changed_files/withLimit50/"
-
-    prWithYear
-      .where("changed_files <= 50")
-      .write
-      .format("qbeast")
-      .option("columnsToIndex", columnsToIndex)
-      .save(doubleLimitedPath)
+  def groupSampleSize(cnt: Long, avg: Double, std: Double, tol: Double): Double = {
+    if (cnt <= 30d) {
+      1.0
+    } else {
+      val z = 1.96 // 95% confident
+      val central = z * std / avg
+      val frac = (1.0 / (tol * tol)) * central * central / cnt
+      frac
+    }
   }
 
-  def writeWithLimit(imp: String, dcs: String, targetColumn: String, upperBound: Int): Unit = {
+  val samplingFraction =
+    udf((cnt: Long, avg: Double, std: Double, tol: Double) => groupSampleSize(cnt, avg, std, tol))
+
+  spark.udf.register("samplingFraction", samplingFraction)
+
+  def writeWithLimit(
+      pr: DataFrame,
+      imp: String,
+      dcs: String,
+      targetColumn: String,
+      upperBound: Int): Unit = {
     val isValidImp = Seq("double", "piecewiseSeq", "single").contains(imp)
-    val isValidTargetColumn =
-      Seq("changed_files", "added_rows", "deleted_rows").contains(targetColumn)
+    val isValidTargetColumn = targetColumns.contains(targetColumn)
 
     assert(isValidImp && isValidTargetColumn, "Input not valid, try again")
 
@@ -53,61 +51,120 @@ class ToleranceScript(spark: SparkSession) {
       .save(targetPath)
   }
 
+  def computeGroupFractions(
+      sourcePath: String,
+      fractionPath: String,
+      columnName: String): Unit = {
+    val pr = spark.read.format("qbeast").load(sourcePath)
+
+    pr.createOrReplaceTempView("pr")
+
+    val statsDf_limit50 = spark.sql(s"""
+         |SELECT
+         |repo_main_language AS language,
+         |year,
+         |COUNT($columnName) AS cnt,
+         |AVG($columnName) AS avg,
+         |STDDEV_POP($columnName) AS std,
+         |'$columnName'
+         |FROM
+         |pr
+         |GROUP BY
+         |repo_main_language, year
+         |""".stripMargin)
+
+    statsDf_limit50.createOrReplaceTempView("stats")
+
+    val groupSamplingFraction = spark.sql(s"""
+          |SELECT
+          |language,
+          |year,
+          |avg, std, cnt,
+          |samplingFraction(cnt, avg, std, 0.1) AS 10p_fraction,
+          |samplingFraction(cnt, avg, std, 0.01) AS 1p_fraction,
+          |'$columnName'
+          |FROM
+          |stats
+          |""".stripMargin)
+
+    groupSamplingFraction.show()
+
+    groupSamplingFraction.write.option("header", "true").csv(fractionPath)
+
+  }
+
+//  val sourcePath = "s3a://qbeast-research/github-archive/qbeast-pr/double/changed_files_upperBound50/5000000/"
+//  val fractionPath = "s3a://qbeast-research/github-archive/qbeast-pr/double/changed_files_upperBound50/groupFractions/"
+
+  def computeFractionFiles(sourcePath: String, fractionPath: String): Seq[RowWithPaths] = {
+    val extractor = new FileExtractor(spark, sourcePath)
+    val groupFractions = (spark.read
+      .format("csv")
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .load(fractionPath))
+
+    val rows = groupFractions.collect()
+    var numMBs = 0L
+    rows.map {
+      case Row(
+            language: String,
+            year: Int,
+            avg: Double,
+            std: Double,
+            cnt: Int,
+            fraction10p: Double,
+            fraction1p: Double,
+            col: String) =>
+        val filterGroup = s"repo_main_language == '$language' AND year == $year"
+        val (paths10p, numMBs10p) = getFilePaths(fraction10p, extractor, filterGroup, sourcePath)
+        val (paths1p, numMBs1p) = getFilePaths(fraction1p, extractor, filterGroup, sourcePath)
+
+        numMBs += numMBs10p + numMBs1p
+
+        RowWithPaths(
+          language,
+          year,
+          avg,
+          std,
+          cnt,
+          fraction10p,
+          fraction1p,
+          col,
+          paths10p,
+          paths1p)
+    }
+  }
+
+  def getFilePaths(
+      fraction: Double,
+      extractor: FileExtractor,
+      filterGroup: String,
+      sourcePath: String): (Seq[String], Long) = {
+    val blocks = extractor.getSamplingBlocks(fraction, Seq(expr(filterGroup).expr))
+    val filePaths = blocks.map(sourcePath + _.path)
+    val numMBs = blocks.map(_.size / 1024 / 1024).sum
+    (filePaths, numMBs)
+  }
+
+//    val df = spark.read.parquet(filePaths: _*).where(filterGroup)
+//    val sampleAvg = df
+//      .agg(avg("changed_files"))
+//      .collect()
+//      .head
+//      .getDouble(0)
+//    val error = Math.abs(sampleAvg - totalAvg) / totalAvg
+//    (sampleAvg, error, sizeInMB)
 }
 
-//
-// val path = "s3a://qbeast-research/github-archive/pr_twoCols/"
-//
-// def groupSampleSize(cnt: Long, avg: Double, std: Double): Double = {
-//  val tolerance = 0.1
-//  val z = 1.96 // 95% confident
-//  val central = z * std / avg
-//  val frac = (1.0 / (tolerance * tolerance)) * central * central / cnt
-//  frac
-//  }
-//
-//  val samplingFraction =
-//  udf((cnt: Long, avg: Double, std: Double) => groupSampleSize(cnt, avg, std))
-//
-//  spark.udf.register("samplingFraction", samplingFraction)
-//
-//  def getTargetColumnValues(columnName: String): Unit = {
-//  val df = spark.read.format("qbeast").load(path)
-//  df.createOrReplaceTempView("pr")
-//
-//  //    val columnName = "changed_files"
-//  val statsDf_limit50 = spark.sql(s"""SELECT
-//        |repo_main_language AS language,
-//        |YEAR(to_date(month_year, 'MM-yyyy')) AS year,
-//        |COUNT($columnName) AS cnt,
-//        |AVG($columnName) AS avg,
-//        |STD($columnName) AS std
-//        |FROM
-//        |pr
-//        |WHERE
-//        |$columnName <= 50 AND YEAR(to_date(month_year, 'MM-yyyy')) IS NOT NUll
-//        |GROUP BY
-//        |repo_main_language, YEAR(to_date(month_year, 'MM-yyyy'))
-//        |HAVING cnt > 30
-//        |""".stripMargin)
-//
-//  statsDf_limit50.createOrReplaceTempView("stats")
-//
-//  val groupSamplingFraction = spark.sql(s"""
-//        |SELECT
-//        |language,
-//        |year,
-//        |samplingFraction(cnt, avg, std) AS fraction,
-//        |avg, std, cnt, '$columnName'
-//        |FROM
-//        |stats
-//        |""".stripMargin)
-//
-//  groupSamplingFraction.show()
-//  //    groupSamplingFraction
-//  //      .agg(
-//  //        count("*").alias("cnt"),
-//  //        avg("fraction").alias("avg"),
-//  //        percentile_approx(col("fraction"), lit(0.5), lit(10000000)).alias("median"))
-//  //      .show()
-//  }
+case class RowWithPaths(
+    language: String,
+    year: Int,
+    avg: Double,
+    std: Double,
+    cnt: Int,
+    fraction10p: Double,
+    fraction1p: Double,
+    changed_files: String,
+    paths10p: Seq[String],
+    paths1p: Seq[String])
