4badd874b1c3c46ff90e746c3ea52f2d9a26a882
jiaweihu <jiaweihu@jiaweihus-MacBook-Air.local>
2022-06-14 18:54:47 +0200

    Add script for error computation

--- src/main/scala/io/qbeast/ToleranceScript.scala
+++ src/main/scala/io/qbeast/ToleranceScript.scala
@@ -3,9 +3,12 @@
  */
 package io.qbeast
 
+import io.qbeast.core.model.QbeastBlock
 import io.qbeast.spark.index.query.FileExtractor
-import org.apache.spark.sql.functions.{expr, udf}
+import org.apache.spark.sql.functions.{avg, expr, udf}
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+
+import scala.collection.mutable
 
 // scalastyle:off
 class ToleranceScript(spark: SparkSession) {
@@ -93,8 +96,13 @@ class ToleranceScript(spark: SparkSession) {
 
   }
 
-//  val sourcePath = "s3a://qbeast-research/github-archive/qbeast-pr/double/changed_files_upperBound50/5000000/"
-//  val fractionPath = "s3a://qbeast-research/github-archive/qbeast-pr/double/changed_files_upperBound50/groupFractions/"
+//  def getFilePaths(
+//      fraction: Double,
+//      extractor: FileExtractor,
+//      filterGroup: String): Seq[QbeastBlock] = {
+//    val blocks = extractor.getSamplingBlocks(fraction, Seq(expr(filterGroup).expr))
+//    blocks
+//  }
 
   def computeFractionFiles(sourcePath: String, fractionPath: String): Seq[RowWithPaths] = {
     val extractor = new FileExtractor(spark, sourcePath)
@@ -105,22 +113,34 @@ class ToleranceScript(spark: SparkSession) {
       .load(fractionPath))
 
     val rows = groupFractions.collect()
-    var numMBs = 0L
-    rows.map {
+    val allBlocks10p = mutable.ArrayBuffer.empty[QbeastBlock]
+    val allBlocks1p = mutable.ArrayBuffer.empty[QbeastBlock]
+    val fractionFiles = rows.map {
       case Row(
-            language: String,
-            year: Int,
+            language,
+            year,
             avg: Double,
             std: Double,
             cnt: Int,
             fraction10p: Double,
             fraction1p: Double,
             col: String) =>
-        val filterGroup = s"repo_main_language == '$language' AND year == $year"
-        val (paths10p, numMBs10p) = getFilePaths(fraction10p, extractor, filterGroup, sourcePath)
-        val (paths1p, numMBs1p) = getFilePaths(fraction1p, extractor, filterGroup, sourcePath)
+        val groupFilter = (language, year) match {
+          case (l: String, y: Int) => s"""repo_main_language == "$l" AND year == $y"""
+          case (null, y: Int) => s"repo_main_language IS NULL AND year == $y"
+          case (l: String, null) => s"""repo_main_language == "$l" AND year IS NULL"""
+          case (null, null) => s"repo_main_language IS NULL AND year IS NULL"
+          case _ => {
+            println(">>> Shit happening")
+            "Shit happening"
+          }
+        }
+        val expressions = Seq(expr(groupFilter).expr)
+        val blocks10p = extractor.getSamplingBlocks(fraction10p, expressions)
+        val blocks1p = extractor.getSamplingBlocks(fraction1p, expressions)
 
-        numMBs += numMBs10p + numMBs1p
+        allBlocks10p ++= blocks10p
+        allBlocks1p ++= blocks1p
 
         RowWithPaths(
           language,
@@ -131,35 +151,73 @@ class ToleranceScript(spark: SparkSession) {
           fraction10p,
           fraction1p,
           col,
-          paths10p,
-          paths1p)
+          blocks10p.map(sourcePath + _.path),
+          blocks1p.map(sourcePath + _.path),
+          groupFilter)
+    }
+
+    val files10p = fractionFiles.flatMap(r => r.paths10p).toSet.toSeq
+    val unique10pBlocks = allBlocks10p.toSet.toSeq
+    val size10p = (unique10pBlocks.foldLeft(0d) { case (acc, b) =>
+      acc + b.size / 1024d / 1024d
+    } / 1024d).toInt
+
+    val files1p = fractionFiles.flatMap(r => r.paths1p).toSet.toSeq
+    val unique1pBlocks = allBlocks1p.toSet.toSeq
+    val size1p = (unique1pBlocks.foldLeft(0d) { case (acc, b) =>
+      acc + b.size / 1024d / 1024d
+    } / 1024d).toInt
+
+    val areTheSame =
+      unique10pBlocks.size == unique1pBlocks.size && unique10pBlocks.forall(
+        unique1pBlocks.contains)
+    print(s"Are the same blocks? $areTheSame")
+    println(s"Number of files read for t=10%: ${files10p.size}, total size: $size10p")
+    println(s"Number of files read for t=1%: ${files1p.size}, total size: $size1p")
+
+    fractionFiles
+  }
+
+  def addApproximationError(fractionFiles: Seq[RowWithPaths]): Seq[RowWithPaths] = {
+    fractionFiles.map {
+      case r @ RowWithPaths(_, _, average, _, _, _, _, _, paths10p, paths1p, groupFilter, _, _) =>
+        r.copy(
+          avgError10p = computeAvgError(paths10p, groupFilter, average),
+          avgError1p = computeAvgError(paths1p, groupFilter, average))
     }
   }
 
-  def getFilePaths(
-      fraction: Double,
-      extractor: FileExtractor,
-      filterGroup: String,
-      sourcePath: String): (Seq[String], Long) = {
-    val blocks = extractor.getSamplingBlocks(fraction, Seq(expr(filterGroup).expr))
-    val filePaths = blocks.map(sourcePath + _.path)
-    val numMBs = blocks.map(_.size / 1024 / 1024).sum
-    (filePaths, numMBs)
+  def computeAvgError(sampleFiles: Seq[String], groupFilter: String, average: Double): Double = {
+    println(s"Number of files: ${sampleFiles.length}")
+    val sample = spark.read.format("parquet").load(sampleFiles: _*)
+    val averageDf = sample.where(groupFilter).agg(avg("changed_files"))
+    val outputRow = averageDf.collect().head
+    if (outputRow.isNullAt(0)) {
+      averageDf.show()
+      -1
+    } else {
+      val sampleAvg = outputRow.getDouble(0)
+      Math.abs(average - sampleAvg) / average
+    }
   }
 
-//    val df = spark.read.parquet(filePaths: _*).where(filterGroup)
-//    val sampleAvg = df
-//      .agg(avg("changed_files"))
-//      .collect()
-//      .head
-//      .getDouble(0)
-//    val error = Math.abs(sampleAvg - totalAvg) / totalAvg
-//    (sampleAvg, error, sizeInMB)
+  def main(args: Set[String]): Unit = {
+    import spark.implicits._
+    val sourcePath =
+      "s3a://qbeast-research/github-archive/qbeast-pr/double/changed_files_upperBound50/5000000/"
+    val fractionPath =
+      "s3a://qbeast-research/github-archive/qbeast-pr/double/changed_files_upperBound50/groupFractions/"
+
+    val fractionFiles = computeFractionFiles(sourcePath, fractionPath)
+    val withEstimationError = addApproximationError(fractionFiles)
+    withEstimationError.toDS
+  }
+
 }
 
 case class RowWithPaths(
-    language: String,
-    year: Int,
+    language: Any,
+    year: Any,
     avg: Double,
     std: Double,
     cnt: Int,
@@ -167,4 +225,7 @@ case class RowWithPaths(
     fraction1p: Double,
     changed_files: String,
     paths10p: Seq[String],
-    paths1p: Seq[String])
+    paths1p: Seq[String],
+    groupFilter: String,
+    avgError10p: Double = 0,
+    avgError1p: Double = 0)
